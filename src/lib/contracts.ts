ce109807fef4bf5c82b8942e3d51c64beb523e5f
MBThePro <131933737+MBThePro@users.noreply.github.com>
2025-08-08 15:57:34 +0700

    Update factory address in contracts.ts to new deployment address

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -3,7 +3,7 @@ import CrowdfundingAbi from '../frontend-abis/Crowdfunding.json';
 import { createPublicClient, http } from 'viem';
 import { sepolia } from 'wagmi/chains';
 
-export const factoryAddress = '0x6A30cEbB2e60314E6eD5Cb0e3B022681D14469E3';
+export const factoryAddress = '0x5254E6d6b4a2980699B7052F2D303a311873a9D1';
 
 export const publicClient = createPublicClient({
     chain: sepolia,
